75c8442a93e26d04225b7a7c22b33ff8b7c50ef1
Michael Phelps <michael.phelps@cpcc.edu>
2021-06-04 21:45:23 -0400

    Revise unit tests to be compatible with pytest

--- pythagoras/tests/test_core.py
+++ pythagoras/tests/test_core.py
@@ -3,11 +3,12 @@ from unittest import TestCase
 from pythagoras import python_to_go
 
 
-def test_example(test_case: TestCase, example: str):
-    with open(f"examples/{example}.py") as a, open(f"examples/{example}.go") as b:
+def assert_examples_match(test_case: TestCase, example: str):
+    with open(f"./pythagoras/tests/examples/{example}.py") as a, \
+            open(f"./pythagoras/tests/examples/{example}.go") as b:
         test_case.assertEqual(python_to_go(a.read()), b.read())
 
 
 class Test(TestCase):
     def test_hello_world(self):
-        test_example(self, "helloworld")
+        assert_examples_match(self, "helloworld")
