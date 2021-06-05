6a4bec111ded5c62e929ac9865a8960a8d9c4eb8
Michael Phelps <michael.phelps@cpcc.edu>
2021-06-04 22:38:04 -0400

    Use gofmt on output of code

--- pythagoras/core.py
+++ pythagoras/core.py
@@ -1,10 +1,29 @@
+import ast
+from subprocess import Popen, PIPE
+
+# from pythagoras import go_ast
+
+
 def python_to_go(python: str) -> str:
-    return """\
+    tree = ast.parse(python)
+    # go_code = go_ast.unparse(tree)
+    return _gofmt("""\
 package main
-
 import "fmt"
-
 func main() {
 	fmt.Println("hello world")
 }
-"""
+""")
+
+
+def _gofmt(code: str) -> str:
+    p = Popen(["gofmt", "-s"], stdout=PIPE, stdin=PIPE)
+    out, err = p.communicate(code.encode())
+    return out.decode()
+
+
+# Debugging
+if __name__ == '__main__':
+    print(python_to_go("""\
+print("Hello world")
+"""))
