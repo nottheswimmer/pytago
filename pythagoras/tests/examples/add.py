d61021818962c5e359f746272868290c0082df8b
Michael Phelps <michael.phelps@cpcc.edu>
2021-06-04 21:58:39 -0400

    Add (currently failing) test case for addition and rework test suite to handle directories better

---
+++ pythagoras/tests/examples/add.py
@@ -0,0 +1,10 @@
+def main():
+    print(add(2, 2))
+
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+if __name__ == '__main__':
+    main()
--- pythagoras/tests/test_core.py
+++ pythagoras/tests/test_core.py
@@ -1,14 +1,22 @@
+import os
 from unittest import TestCase
 
 from pythagoras import python_to_go
 
+# Change directories to the location of the test for consistency between testing environments
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
 
 def assert_examples_match(test_case: TestCase, example: str):
-    with open(f"./pythagoras/tests/examples/{example}.py") as a, \
-            open(f"./pythagoras/tests/examples/{example}.go") as b:
+    with open(f"examples/{example}.py") as a, \
+            open(f"examples/{example}.go") as b:
         test_case.assertEqual(python_to_go(a.read()), b.read())
 
 
 class Test(TestCase):
     def test_hello_world(self):
         assert_examples_match(self, "helloworld")
+
+    def test_add(self):
+        assert_examples_match(self, "add")
