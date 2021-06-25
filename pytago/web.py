6418dc9f8daa8d2f787c8d76bd9a05c512c86f9d
Michael Phelps <michael.phelps@cpcc.edu>
2021-06-24 22:53:23 -0400

    Enable CORS for web-app

--- pytago/web.py
+++ pytago/web.py
@@ -1,9 +1,11 @@
 import os
 
 from flask import Flask, request
+from flask_cors import CORS
 from pytago.core import python_to_go
 
 app = Flask(__name__)
+cors = CORS(app)
 
 
 @app.route("/", methods=["POST"])
@@ -11,8 +13,20 @@ def index():
     py = (request.get_json() or {}).get("py")
     if not py:
         return "Bad request", 400
-    return python_to_go(py, app.debug)
-
+    try:
+        go = python_to_go(py, app.debug)
+        return go
+    except SyntaxError as e:
+        import traceback
+        tb = traceback.format_exc()
+        lines = []
+        seen_unknown = False
+        for line in tb.splitlines():
+            if '<unknown>' in line:
+                seen_unknown = True
+            if seen_unknown:
+                lines.append(line)
+        return '\n'.join(lines) or str(e)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
